b8852df813e99e3ab733d52303fc0f4e1af7b07b
Carlo Fernando <ca.fernando415@gmail.com>
2018-09-08 16:05:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+function getFirstSelector (selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget () {
+  return document.getElementById("nested").querySelector("div.target");
+}
+function increaseRankBy (n) {
+  const rankedList = document.querySelectorAll('.ranked-list');
+  let i = 0;
+  while(i < rankedList.length) {
+    for (let j = 0; j < rankedList[i].children.length; j++) {
+      rankedList[i].children[j].innerHTML = (parseInt(rankedList[i].children[j].innerHTML) + n);
+    }
+    i++;
+  }
+}
+function deepestChild () {
+  var deepChild = document.getElementById("grand-node").querySelectorAll("div");
+  return deepChild[deepChild.length - 1];
+}
